d9dd15d25d6b9c17eee84200e3534d2b6a20ae01
Nakamiiis <lucianadalgonzalez@gmail.com>
2025-04-02 14:31:52 +0200

    SAveNotFix

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -24,6 +24,8 @@ public class Dead : MonoBehaviour
     }
 
 
+//Aparte de OnTrigger se puede hacer usando el OnCollision para que ocurra al colisionar
+//También se pueden crear tags en el objeto y llamar a esos tags en vez de al nombre de ese objeto
     void OnTriggerEnter2D(Collider2D col){
         Debug.Log(col.name);
 
---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+
+
+GameObject Respawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // spawn = GameObject.Find("Respawn");
+
+
+    }
+
+    /*void OnTriggerEnter2D(Collider2D col){
+        Debug.Log(col.name);
+
+        if(col.name == "Rogue"){
+             Respawn.trasnform.positions = Save.transform.postion;
+        } 
+
+    }
+
+    public void Respawnear(){
+    
+    //Debug.Log("Vidas: "+GameManager.vidas);
+    GameManager.vidas = GameManager.vidas + 1;
+    Debug.Log("Vidas: "+GameManager.vidas);
+
+  }*/
+}
