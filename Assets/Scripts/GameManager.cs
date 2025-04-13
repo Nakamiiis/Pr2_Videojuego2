9071a08c4b9918746d7eb27ff11b05ce1fed4cec
Nakamiiis <lucianadalgonzalez@gmail.com>
2025-04-13 11:09:09 +0200

    InterfazInicio

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,18 +14,30 @@ public static bool iAmDead = false;
 
 public static int muertes = 0;
 
+private GameObject vidasTexto;
+private GameObject monedasTexto;
+private GameObject puntosTexto;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        vidasTexto = GameObject.Find("VidasTexto");
+        monedasTexto = GameObject.Find("MonedasTexto");
+        puntosTexto = GameObject.Find("PuntosTexto");
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log("Monedas: "+GameManager.marcador);
+
+        vidasTexto.GetComponent<TextMeshProUGUI>().text = vidas.ToString();
+        monedasTexto.GetComponent<TextMeshProUGUI>().text = marcador.ToString();
+        puntosTexto.GetComponent<TextMeshProUGUI>().text = muertes.ToString();
 
 
     }
--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -11,24 +11,34 @@ public class Parallax : MonoBehaviour
 
     public float velocidadParallax = 1;
 
+    public Vector3 posInicial;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         camera = GameObject.FindWithTag("MainCamera");
+        posInicial = transform.position;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+      /* transform.position = posInicial + camera.transform.position;
+       transform.position = new Vector3(
+        posInicial.x + (camera.transform.position.x * velocidadParallax),
+        posInicial.y,
+        0);*/
 
     }
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(
+       
+       
+       
+       transform.position = new Vector3(
             camera.transform.position.x/velocidadParallax, camera.transform.position.y/velocidadParallax, 0);
 
 
